c05094728088701bc0ad34ee0abdd53be98d20b5
Azal-Amer <58004047+Azal-Amer@users.noreply.github.com>
2021-01-12 22:49:24 -0600

    Added assignment for 1/13/21

---
+++ Credit Card.py
@@ -0,0 +1,12 @@
+
+card = input("Put in your credit card number ")
+type = int(card[0])
+
+if type == 3:
+    print(" you have an american express card")
+if type == 4:
+    print("you have a visa card")
+if type == 5:
+    print("you have a mastercard")
+if type == 6:
+    print("you have a discover card")
--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("hello data science")
 kitten = input()
+print("What's ya name m8?")
+name = input("whatcha nameh")
 if(kitten == "hi"):
-    print("ew gross")
+    print("ew gross I hate you, " + name)
 
+# 
