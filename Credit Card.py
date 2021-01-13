48f33801f31b4b004a4f21d301b0a13262e34799
Azal-Amer <58004047+Azal-Amer@users.noreply.github.com>
2021-01-12 22:50:59 -0600

    Small bug fix

--- Credit Card.py
+++ Credit Card.py
@@ -4,9 +4,11 @@ type = int(card[0])
 
 if type == 3:
     print(" you have an american express card")
-if type == 4:
+elif type == 4:
     print("you have a visa card")
-if type == 5:
+elif type == 5:
     print("you have a mastercard")
-if type == 6:
+elif type == 6:
     print("you have a discover card")
+else:
+    print("this is not a supported card")
