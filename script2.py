895815601ceba5e9c245fb0dfddf1dcb793a4a9b
Marisua <31741111+GreenhillTeacher@users.noreply.github.com>
2021-11-23 11:44:27 -0600

    testing

---
+++ script2.py
@@ -0,0 +1,28 @@
+#import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import CSVs:
+user_data = pd.read_csv("user_data.csv")
+pop_data = pd.read_csv("pop_data.csv")
+
+# Merged tables with location data:
+new_df = pd.merge(user_data, pop_data)
+new_df.loc[new_df.population_proper < 100000, "location"] = "rural"
+new_df.loc[new_df.population_proper >= 100000, "location"] = "urban"
+
+# Paste histogram code:
+age = new_df["age"]
+sns.displot(age)
+ 
+plt.show() 
+
+# Paste mean age location code:
+
+
+# Paste barplot code:
+
+
+# Paste violinplot code:
+
